3751e8c6da6f2de78655114e2f25219f34c69763
Yan Zhang <663yan@gmail.com>
2019-05-01 21:42:02 -0400

    Do not remove input's CRLF

--- lib/html_render.dart
+++ lib/html_render.dart
@@ -4,7 +4,6 @@ import 'package:html/dom.dart' as dom;
 import 'package:html/parser.dart' as parser;
 
 Widget htmlParse(String data) {
-  data = data.replaceAll('\r\n\r\n', '');
   dom.Document document = parser.parse(data);
   Widget widgeList = _parseNode(document.body);
   return widgeList;
